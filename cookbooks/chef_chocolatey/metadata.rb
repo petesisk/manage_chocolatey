9c595ad28c42a0320c33f57cecfbdc9e42a1198f
Peter Sisk <psisk@dropbox.com>
2020-12-18 11:18:48 -0800

    cookbook created

---
+++ cookbooks/chef_chocolatey/metadata.rb
@@ -0,0 +1,11 @@
+name 'chef_chocolatey'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures chef_chocolatey'
+version '0.1.0'
+chef_version '>= 15.0'
+issues_url 'https://github.com/petesisk/chef_chocolatey/issues'
+source_url 'https://github.com/petesisk/chef_chocolatey'
+
+depends 'chocolatey'
---
+++ cookbooks/chef_chocolatey/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef_chocolatey
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
