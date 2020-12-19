ed97e9957b3564deefaa5db11506b940f246c1ac
Peter Sisk <psisk@dropbox.com>
2020-12-19 12:07:30 -0800

    add ohai plugin & managed_updates

--- cookbooks/chef_chocolatey/recipes/default.rb
+++ cookbooks/chef_chocolatey/recipes/default.rb
@@ -6,6 +6,10 @@
 
 include_recipe 'chocolatey'
 
+ohai_plugin 'choco_list'
+
+log node['choco_list']
+
 node['chef_chocolatey']['installs'].each do |name|
   chocolatey_package name do
     action :install
@@ -16,6 +20,14 @@ end
 node['chef_chocolatey']['updates'].each do |name|
   chocolatey_package name do
     action :install
+    returns [0, 3010]
+  end
+end
+
+node['chef_chocolatey']['managed_updates'].each do |name|
+  chocolatey_package name do
+    action :install
+    only_if { node['choco_list']['packages'].include?(name) } 
     returns [0, 3010]
   end
 end
