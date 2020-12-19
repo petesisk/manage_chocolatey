27df668830d0f2ade126b754bc51417c22f399b8
petesisk <69913599+petesisk@users.noreply.github.com>
2020-12-19 13:23:18 -0800

    Update default.rb

--- cookbooks/chef_chocolatey/recipes/default.rb
+++ cookbooks/chef_chocolatey/recipes/default.rb
@@ -4,30 +4,5 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
-include_recipe 'chocolatey'
-
-ohai_plugin 'chocolist'
-
-log node['chocolist']['packages']
-
-node['chef_chocolatey']['installs'].each do |name|
-  chocolatey_package name do
-    action :install
-    returns [0, 3010]
-  end
-end
-
-node['chef_chocolatey']['updates'].each do |name|
-  chocolatey_package name do
-    action :install
-    returns [0, 3010]
-  end
-end
-
-node['chef_chocolatey']['managed_updates'].each do |name|
-  chocolatey_package name do
-    action :install
-    only_if { node['chocolist']['packages'].include?(name) } 
-    returns [0, 3010]
-  end
-end
+include_recipe 'chef_chocolatey::install'
+include_recipe 'chef_chocolatey::packages'
