bda747c270819916f810149c533191cbb1843492
petesisk <69913599+petesisk@users.noreply.github.com>
2020-12-19 11:24:46 -0800

    Update default.rb

--- cookbooks/chef_chocolatey/recipes/default.rb
+++ cookbooks/chef_chocolatey/recipes/default.rb
@@ -5,3 +5,17 @@
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
 include_recipe 'chocolatey'
+
+node['chef_chocolatey']['installs'].each do |name|
+  chocolatey_package name do
+    action :install
+    returns [0, 3010]
+  end
+end
+
+node['chef_chocolatey']['updates'].each do |name|
+  chocolatey_package name do
+    action :install
+    returns [0, 3010]
+  end
+end
