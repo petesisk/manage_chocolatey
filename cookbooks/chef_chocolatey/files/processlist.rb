7c897fce34eb0596a25587a224c0ce5b2eb8878e
petesisk <69913599+petesisk@users.noreply.github.com>
2020-12-26 23:27:28 -0800

    Update processlist.rb

--- cookbooks/chef_chocolatey/files/processlist.rb
+++ cookbooks/chef_chocolatey/files/processlist.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-Ohai.plugin :Processlist do
+Ohai.plugin :processlist do
   provides 'processlist'
 
   collect_data(:windows) do
     processlist Mash.new
+    processlist['names'] = []
 
-    cmd = Mixlib::ShellOut.new('TASKLIST /v /fi "STATUS eq running"')
+    cmd = Mixlib::ShellOut.new('TASKLIST /s')
     cmd.run_command
-    processlist = cmd.stdout
+    processlist['names'] = cmd.stdout
   end
 end
+# TASKLIST /v /fi "STATUS eq running"
