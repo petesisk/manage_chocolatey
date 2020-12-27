f0a2488e4d0ee38da6f2a3230ef08deeb06929da
petesisk <69913599+petesisk@users.noreply.github.com>
2020-12-26 23:57:19 -0800

    Update packages.rb

--- cookbooks/chef_chocolatey/recipes/packages.rb
+++ cookbooks/chef_chocolatey/recipes/packages.rb
@@ -1,7 +1,7 @@
 ohai_plugin 'chocolist'
 ohai_plugin 'processlist'
 
-log node['processlist']['names']
+log node['processlist']
 
 node['chef_chocolatey']['installs'].each do |name|
   chocolatey_package name do
