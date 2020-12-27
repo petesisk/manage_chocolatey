4b4b962e023feb7f007afa231e3f8168bba9714f
petesisk <69913599+petesisk@users.noreply.github.com>
2020-12-27 12:47:51 -0800

    Update configure.rb

--- cookbooks/chef_chocolatey/recipes/configure.rb
+++ cookbooks/chef_chocolatey/recipes/configure.rb
@@ -9,3 +9,22 @@ node['chef_chocolatey']['disabled_features'].each do |feature|
     action :disable
   end
 end
+
+if node['chef_chocolatey']['private_feed'] == true
+  chocolatey_source 'chocolatey' do
+    source 'https://chocolatey.org/api/v2/'
+    action :add
+  end
+else
+  chocolatey_source 'chocolatey' do
+    source 'https://chocolatey.org/api/v2/'
+    action :add
+  end
+end
+
+if node['chef_chocolatey']['disable_public_feed'] == true
+  chocolatey_source 'chocolatey' do
+    source 'https://chocolatey.org/api/v2/'
+    action :disable
+  end
+end
