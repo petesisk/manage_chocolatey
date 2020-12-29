ae5295b60d662ca51433c40bcd2e22ce53b1dd3d
petesisk <69913599+petesisk@users.noreply.github.com>
2020-12-29 08:38:39 -0800

    Update manage_chocolatey_packages.rb

--- resources/manage_chocolatey_packages.rb
+++ resources/manage_chocolatey_packages.rb
@@ -1,6 +1,10 @@
 resource_name :manage_chocolatey_packages
 provides :manage_chocolatey_packages, :os => 'windows'
 default_action :manage
+property :installs, [TrueClass, FalseClass], default: true
+property :updates, [TrueClass, FalseClass], default: true
+property :managed_updates, [TrueClass, FalseClass], default: true
+property :safe_updates, [TrueClass, FalseClass], default: true
 
 action :manage do
   return unless platform_family?('windows')
@@ -13,6 +17,7 @@ action :manage do
       chocolatey_package name do
         action :install
         returns return_codes
+        not_if new_resource.installs == false
       end
     end
   end
@@ -23,6 +28,7 @@ action :manage do
       chocolatey_package name do
         action :upgrade
         returns return_codes
+        not_if new_resource.updates == false
       end
     end
   end
@@ -34,6 +40,7 @@ action :manage do
         action :upgrade
         only_if { node['chocolist']['packages'].include?(name) }
         returns return_codes
+        not_if new_resource.managed_updates == false
       end
     end
   end
@@ -45,6 +52,7 @@ action :manage do
         action :upgrade
         not_if { node['processlist']['names'].include?(process) }
         returns return_codes
+        not_if new_resource.safe_updates == false
       end
     end
   end
