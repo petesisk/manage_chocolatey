ef573f78c4d1e71c8352fddbd562f88a80ed24ae
petesisk <69913599+petesisk@users.noreply.github.com>
2020-12-29 09:32:14 -0800

    Update packages.rb

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,39 +1,9 @@
 ohai_plugin 'chocolist'
 ohai_plugin 'processlist'
 
-# node['chocolatey_packages']['installs'].each do |name|
-#   chocolatey_package name do
-#     action :install
-#     returns [0, 3010]
-#   end
-# end
-
-# node['chocolatey_packages']['updates'].each do |name|
-#   chocolatey_package name do
-#     action :upgrade
-#     returns [0, 3010]
-#   end
-# end
-
 manage_chocolatey_packages 'magic' do
   installs true
   updates true
   managed_updates true
   safe_updates false
 end
-
-# node['chocolatey_packages']['managed_updates'].each do |name|
-#   chocolatey_package name do
-#     action :upgrade
-#     only_if { node['chocolist']['packages'].include?(name) }
-#     returns [0, 3010]
-#   end
-# end
-
-# node['chocolatey_packages']['safe_updates'].each do |name, process|
-#   chocolatey_package name do
-#     action :upgrade
-#     not_if { node['processlist']['names'].include?(process) }
-#     returns [0, 3010]
-#   end
-# end
